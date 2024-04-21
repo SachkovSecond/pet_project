5c9db5975deb8d5a70d2bc7e7bebcdaec19c949c
Artemiy <artemiy1astanin@gmail.com>
2024-04-21 19:58:59 +0300

    hz

---
+++ PetProject/PetProject/Configurations/PostConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetProject.Models;
+
+namespace PetProject.Configurations;
+
+public class PostConfiguration : IEntityTypeConfiguration<Post>
+{
+    public void Configure(EntityTypeBuilder<Post> builder)
+    {
+        builder.HasKey(p => p.PostId);
+        builder.Property(p => p.PostName).IsRequired();
+        builder.Property(p => p.PostDescription).IsRequired();
+    }
+}
